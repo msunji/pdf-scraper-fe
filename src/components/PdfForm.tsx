5751a53f38473461330d2f8b07a4c3a64fd649b7
msunji <marge.consunji@gmail.com>
2024-02-26 13:43:18 +0800

    Handle form validation and error messages

--- src/components/PdfForm.tsx
+++ src/components/PdfForm.tsx
@@ -1,10 +1,24 @@
+/* eslint-disable @typescript-eslint/consistent-type-definitions */
 import { 
     useForm, 
-    SubmitHandler 
+    SubmitHandler,
 } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
 
-interface FormData {
-    pdfUrl: string,
+const schema = yup
+    .object({
+        pdfUrl: yup
+            .string()
+            .required(),
+        reportType: yup
+            .string()
+            .required()
+            .matches(/(\.|\/)(pdf)/, 'This form only accepts PDF files')
+    })
+
+type FormData = {
+    pdfUrl: string
     reportType: string
 }
 
@@ -12,10 +26,16 @@ function PdfForm() {
     const {
         register,
         handleSubmit,
-        watch,
         formState: { errors }
-    } = useForm<FormData>();
-    const onSubmit:SubmitHandler<FormData> = (data) => console.log(data);
+    } = useForm<FormData>({
+        defaultValues: {
+            pdfUrl: "",
+            reportType: "",
+        },
+        resolver: yupResolver(schema)
+    });
+    
+    const onSubmit:SubmitHandler<FormData> = (data:FormData) => console.log(data, errors);
 
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
@@ -24,27 +44,40 @@ function PdfForm() {
                 <input
                     id='url'
                     type='text'
-                    defaultValue='pse-report.pdf'
                     {...register('pdfUrl')}
                 />
+                { errors.pdfUrl?.type === 'required' && (
+                    <p>A valid PSE Report URL is required</p>
+                )}
+                { errors.reportType?.type === 'matches' && (
+                    <p>Please input a valid PDF file (e.g. http://pse.com.ph/.../202023-EOD.pdf)</p>
+                ) }
+            </div>
+            <div>
                 <fieldset>
                     <legend>Select a report type</legend>
-                    <div className="radio-container__input">
+                    { errors.reportType?.type === 'required' && (
+                        <p>A valid PSE Report URL is required</p>
+                    )}
+
+                    <div className='radio-container__input'>
                         <input
-                            type="radio"
-                            value="daily"
+                            {...register('reportType')}
+                            type='radio'
+                            value='daily'
                         />
-                        <label htmlFor="daily">
+                        <label htmlFor='daily'>
                             <p>Daily</p>
                             <small>Pick this if you need to scrape daily data for stocks in your portfolio</small>
                         </label>
                     </div>
-                    <div className="radio-container__input">
+                    <div className='radio-container__input'>
                         <input
-                            type="radio"
-                            value="weekly"
+                            {...register('reportType')}
+                            type='radio'
+                            value='weekly'
                         />
-                        <label htmlFor="weekly">
+                        <label htmlFor='weekly'>
                             <p>Weekly</p>
                             <small>Pick this if you need to scrape week-on-week data for the weekly report. You'll need to put in PDF URLs for two different dates separately.</small>
                         </label>
