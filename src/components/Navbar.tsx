6a3269fe8b8cdb9229ec1da433f1119a25e5e225
msunji <marge.consunji@gmail.com>
2024-02-26 11:39:38 +0800

    Make nav component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,12 @@
+function Navbar() {
+    return (
+        <nav>
+            <div className="nav-details">
+            <p>Logged in as: </p>
+            <button className="button">Logout</button>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
