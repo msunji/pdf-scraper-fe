89e2f0245e57de21c46ada45806b660164d21053
msunji <marge.consunji@gmail.com>
2024-02-26 16:27:01 +0800

    Try to fix TS errors

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,25 +1,58 @@
-import { createContext, useEffect } from 'react';
-import { firebaseApp, auth } from '../firebase/firebase';
+/* eslint-disable @typescript-eslint/consistent-type-definitions */
+import { ReactNode, createContext, useState, useEffect } from 'react';
+import firebaseApp from '../firebase/firebase';
+import { getAuth, Auth, User, onAuthStateChanged, signInWithEmailAndPassword } from 'firebase/auth';
 
-type AuthContextType = {
-    currentUser: firebaseApp.User | null;
-
+type AuthProviderProps = {
+    children?: ReactNode
 }
 
-const AuthContext = createContext();
+type AuthContextType = {
+    currentUser: User | null;
+    login: (auth: Auth, email: string, password: string) => Promise<User | void>;
+    logout: () => Promise<void>;
+}
 
-function AuthProvider({ children }) {
-    const [currentUser, setCurrentUser] = useState(null);
+const auth = getAuth(firebaseApp);
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+function AuthProvider({ children }: AuthProviderProps) {
+    const [currentUser, setCurrentUser] = useState<User | null>(null);
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        const unsubscribe = firebase.auth().onAuthStateChanged(setCurrentUser);
+        const unsubscribe = onAuthStateChanged(auth, (user: User | null) => {
+            setCurrentUser(user);
+            setLoading(false);
+        });
+
         return unsubscribe;
-    }, [])
+    }, []);
+
+    const login = async (email: string, password: string) => {
+        try {
+            await signInWithEmailAndPassword(auth, email, password);
+        } catch (error) {
+            console.error(error)
+        }
+    };
+
+    const logout = async () => {
+        await auth.signOut();
+    }
+
+    const value: AuthContextType ={
+        currentUser,
+        login,
+        logout
+    }
 
     return (
         <AuthContext.Provider value={value}>
-
+            { loading ? <p>Loading</p> : children }
         </AuthContext.Provider>
     )
 }
+
+export default AuthProvider;
