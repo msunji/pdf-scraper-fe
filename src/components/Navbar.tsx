639453d9228d9c8f6bdc2d1b35028ba6a667a6ce
msunji <marge.consunji@gmail.com>
2024-02-26 11:51:50 +0800

    Change to single quotes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,9 @@
 function Navbar() {
     return (
         <nav>
-            <div className="nav-details">
+            <div className='nav-details'>
             <p>Logged in as: </p>
-            <button className="button">Logout</button>
+            <button className='button'>Logout</button>
             </div>
         </nav>
     )
