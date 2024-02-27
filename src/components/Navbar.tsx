1456b3ff3aad53e87d13fd942ff51b9a0cbd29bd
msunji <marge.consunji@gmail.com>
2024-02-27 11:13:50 +0800

    Sort out styling and elements

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,16 @@
 function Navbar() {
     return (
         <nav>
-            <ul className='nav-details'>
-                <li>Instructions</li>
-                <li>History</li>
-                <li>
-                    <p>Logged in as: </p>
-                <button className='button'>Logout</button>
-                </li>
-            </ul>
+            <div className='nav-container'>
+                <p className='nav-header'>PSE PDF Scraper</p>
+                <ul className='nav-details'>
+                    <li>Instructions</li>
+                    <li className='nav-details__user'>
+                        <p>email </p>
+                        <button className='button'>Logout</button>
+                    </li>
+                </ul>
+            </div>
         </nav>
     )
 }
