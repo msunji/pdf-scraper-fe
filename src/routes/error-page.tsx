a3dbe7d2bd5141872fae52dbb64e2f6d3e208ed6
msunji <marge.consunji@gmail.com>
2024-02-28 17:15:30 +0800

    Add a back button

--- src/routes/error-page.tsx
+++ src/routes/error-page.tsx
@@ -1,14 +1,18 @@
-import { useRouteError } from 'react-router-dom';
+import { useRouteError, useNavigate } from 'react-router-dom';
 
 function ErrorPage() {
     const error = useRouteError();
-    console.error(error)
+    const navigate = useNavigate();
 
     return (
-        <div>
-            <h1>Whoops.</h1>
-            <p>Looks like this page doesn't exist.</p>
-        </div>
+        <section className="error-routing-section">
+            <div>
+                <h1>Whoops.</h1>
+                <p>Looks like this page doesn't exist.</p>
+            </div>
+
+            <button className="button" onClick={() => navigate(-1)}>Go Back</button>
+        </section>
     )
 }
 
