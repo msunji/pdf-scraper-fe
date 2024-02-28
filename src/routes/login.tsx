513780e715ca6388114e69ee7ee246bad3c6f823
msunji <marge.consunji@gmail.com>
2024-02-28 13:54:51 +0800

    Change page header text

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -4,7 +4,7 @@ function Login() {
     return (
         <main className="login-container">
             <section className="login-form">
-                <h1>Login</h1>
+                <h1>Login to Scraper</h1>
                 <LoginForm />
             </section>
         </main>
