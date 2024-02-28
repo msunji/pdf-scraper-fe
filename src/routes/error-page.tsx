f6bc9ce9e6ddf32c77dcf00c6d551556744193aa
msunji <marge.consunji@gmail.com>
2024-02-28 17:17:12 +0800

    Remove useRouteError import

--- src/routes/error-page.tsx
+++ src/routes/error-page.tsx
@@ -1,7 +1,6 @@
-import { useRouteError, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 function ErrorPage() {
-    const error = useRouteError();
     const navigate = useNavigate();
 
     return (
