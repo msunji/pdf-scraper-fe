d59781f359c59968f466816320a2571ceb1941a9
msunji <marge.consunji@gmail.com>
2024-02-28 09:59:24 +0800

    Move instructions to own route and page

---
+++ src/routes/instructions.tsx
@@ -0,0 +1,11 @@
+function Instructions() {
+    return (
+        <>
+            <section>
+                <h1>Instructions and FAQs</h1>
+            </section>
+        </>
+    )
+}
+
+export default Instructions;
